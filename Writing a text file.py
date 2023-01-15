a2e44de0817d154a7322d08c8fe0995781f99fb6
Pedram Najafizadeh <82384259+pedramnajafi@users.noreply.github.com>
2023-01-15 15:23:30 +0330

    Add files via upload

---
+++ Writing a text file.py
@@ -0,0 +1,14 @@
+text_file = open('inputFile.txt', 'r')
+passed_file = open('passedFile.txt', 'w')
+failed_file = open('failedFile.txt', 'w')
+
+for line in text_file:
+    line_split = line.split()
+    if line_split[2] == 'P':
+        passed_file.write(line)
+    else:
+        failed_file.write(line)
+
+text_file.close()
+passed_file.close()
+failed_file.close()
