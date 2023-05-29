626481b5a1c8d045df151bd3a91e016f8e965719
Pedram Najafizadeh <82384259+pedramnajafi@users.noreply.github.com>
2023-05-29 12:03:47 +0330

    Update Writing a text file.py

--- Writing a text file.py
+++ Writing a text file.py
@@ -1,7 +1,6 @@
 text_file = open('inputFile.txt', 'r')
 passed_file = open('passedFile.txt', 'w')
 failed_file = open('failedFile.txt', 'w')
-
 for line in text_file:
     line_split = line.split()
     if line_split[2] == 'P':
